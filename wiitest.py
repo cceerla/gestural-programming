3656330f1dcef7009d0ef531060cd3c1bf6f23f3
cceerla <cceerla@ucsc.edu>
2026-04-06 18:20:43 -0700

    first few functions written for python file; going smoothly rn

--- wiitest.py
+++ wiitest.py
@@ -1,3 +1,51 @@
-from _xwiimote_cffi import ffi, lib
+from _xwiimote import ffi, lib
 
-print("waow")
+xwii_devices = []
+xwii_iface = []
+
+# prints cffi strings from a list
+def print_strings(ffistrs):
+    for cstr in ffistrs:
+        print(ffi.string(cstr))
+
+# loads global variable xwii_devices with all of the device strings.
+def xw_enumerate():
+    wii_monitor = ffi.new("struct xwii_monitor **")
+    wii_monitor = lib.xwii_monitor_new(False, False)
+    if wii_monitor == ffi.NULL:
+        print("in enumerate(): xwii_monitor_new() returned NULL.")
+
+    while (1):
+        ent = ffi.new("char *")
+        ent = lib.xwii_monitor_poll(wii_monitor)
+        if ent == ffi.NULL:
+            break
+        xwii_devices.append(ent)
+
+def xw_open_ifaces():
+    for path in xwii_devices:
+        iface = ffi.new("struct xwii_iface **")
+        lib.xwii_iface_new(iface, path)
+        lib.xwii_iface_open(iface[0],
+            lib.xwii_iface_available(iface[0]) | lib.XWII_IFACE_WRITABLE)
+        xwii_iface.append(iface[0])
+
+def xw_get_battery(device:int=0):    
+    battery_level = ffi.new("uint8_t *")
+    lib.xwii_iface_get_battery(xwii_iface[device], battery_level)
+    return battery_level[0]
+
+def xw_get_accel(device:int=0):
+    pass
+
+# MAIN
+
+xw_enumerate()
+
+print(f"found devices:")
+print_strings(xwii_devices)
+
+xw_open_ifaces()
+print(f"created interfaces: {xwii_iface}")
+
+print(xw_get_battery(0))
