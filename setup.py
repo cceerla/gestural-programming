19e0db036d0ee16d22e42065d0cbc418f1942910
cceerla <cceerla@ucsc.edu>
2026-04-06 18:19:43 -0700

    made xwiimote cffi build! yay

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='xwiimote',
     version='1',
     py_modules=['xwiimote'],
-    install_requires=['cffi'],
     setup_requires=['cffi'],
     cffi_modules=['xwiimote_build.py:ffibuilder'],
+    install_requires=['cffi'],
 )
